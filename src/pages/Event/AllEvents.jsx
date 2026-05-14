d768f7e49cf599fbdc0566d5ef7c9459a06404aa
Nirvik Roy <nirvikroy802@gmail.com>
2026-05-14 17:56:29 +0530

    final responsive checkup done

--- src/pages/Event/AllEvents.jsx
+++ src/pages/Event/AllEvents.jsx
@@ -14,7 +14,6 @@ const AllEvents = () => {
     const { setBannerTitle } = useBanner();
     useEffect(() => {
         setBannerTitle("Events")
-
     }, [location.pathname])
     const fetchEvent = async () => {
         setloading(true)
